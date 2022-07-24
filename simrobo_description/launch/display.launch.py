a7730411dccee282cf407cacd951b01129d58ce5
YumaMatsumura <yumapine@gmail.com>
2022-07-25 00:04:40 +0900

    add joint_state_publisher_gui

--- simrobo_description/launch/display.launch.py
+++ simrobo_description/launch/display.launch.py
@@ -54,6 +54,12 @@ def generate_launch_description():
         ),
         
         Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        
+        Node(
             package='rviz2',
             executable='rviz2',
             output='screen',

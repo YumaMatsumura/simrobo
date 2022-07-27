4cde700457bb08b9b097e6fdab14fdbbce580370
YumaMatsumura <yumapine@gmail.com>
2022-07-27 13:10:02 +0900

    add odom topic

--- simrobo_driver/include/simrobo_driver/driver_component.hpp
+++ simrobo_driver/include/simrobo_driver/driver_component.hpp
@@ -4,6 +4,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <tf2/LinearMath/Quaternion.h>
 
 using namespace std::placeholders;
 using namespace std::chrono_literals;
@@ -22,15 +24,20 @@ private:
   float velocityToRound(float vel);
   void twistCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
   void updateCallback();
+  void publishOdom(const rclcpp::Time & now, const rclcpp::Duration & duration);
   void publishVelCommand();
   
   rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_servo_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_odom_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_twist_;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Time prev_time;
   
   geometry_msgs::msg::Twist twist_buff;
   float wheel_radius_m_;
   float tread_width_m_;
+  double pos[3];
+  std::string global_frame_id_;
 };
 
 }// namespace simrobo_driver
--- simrobo_driver/src/driver_component.cpp
+++ simrobo_driver/src/driver_component.cpp
@@ -6,14 +6,20 @@ namespace simrobo_driver
 // ========== コンストラクタ ========== //
 Driver::Driver(
   const rclcpp::NodeOptions& options
-): Node("simrobo_driver", options)
+): Node("simrobo_driver", options),
+   pos{0.0, 0.0, 0.0}
 {
+  // initialize
+  prev_time = this->now();
+
   // parameters
   wheel_radius_m_ = this->declare_parameter<float>("wheel_radius_size_m", 0.1);
   tread_width_m_ = this->declare_parameter<float>("tread_width_m", 0.33);
+  global_frame_id_ = this->declare_parameter<std::string>("global_frame_id", "odom");
 
   // publisher, subscriber & timer
   pub_servo_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/velocity_controller/commands", 1);
+  pub_odom_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 1);
   sub_twist_ = this->create_subscription<geometry_msgs::msg::Twist>(
     "cmd_vel", 1, std::bind(&Driver::twistCallback, this, _1)
   );
@@ -38,8 +44,45 @@ void Driver::twistCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
 // ========== Timerのコールバック関数 ========== //
 void Driver::updateCallback()
 {
+  rclcpp::Time current_time = this->now();
+  rclcpp::Duration duration(current_time - prev_time);
+  prev_time = current_time;
+
+  // Odometryの計算
+  publishOdom(current_time, duration);
   // Velocity Commandの計算
   publishVelCommand();
+}
+
+// ========== OdometryをPublishする関数 ========== //
+void Driver::publishOdom(const rclcpp::Time & now, const rclcpp::Duration & duration)
+{
+  double dt_sec = duration.nanoseconds() / 1e9;
+  pos[0] += twist_buff.linear.x * cos(pos[2] + (twist_buff.angular.z * dt_sec / 2.0)) * dt_sec;
+  pos[1] += twist_buff.linear.x * sin(pos[2] + (twist_buff.angular.z * dt_sec / 2.0)) * dt_sec;
+  pos[2] += twist_buff.angular.z * dt_sec;
+
+  tf2::Quaternion q;
+  q.setRPY(0, 0, pos[2]);
+
+  auto odom_msg = nav_msgs::msg::Odometry();
+  odom_msg.header.stamp = now;
+  odom_msg.header.frame_id = global_frame_id_;
+  odom_msg.pose.pose.position.x = pos[0];
+  odom_msg.pose.pose.position.y = pos[1];
+  odom_msg.pose.pose.position.z = 0.0;
+  odom_msg.pose.pose.orientation.x = q.x();
+  odom_msg.pose.pose.orientation.y = q.y();
+  odom_msg.pose.pose.orientation.z = q.z();
+  odom_msg.pose.pose.orientation.w = q.w();
+  odom_msg.twist.twist.linear.x = twist_buff.linear.x;
+  odom_msg.twist.twist.linear.y = 0.0;
+  odom_msg.twist.twist.linear.z = 0.0;
+  odom_msg.twist.twist.angular.x = 0.0;
+  odom_msg.twist.twist.angular.y = 0.0;
+  odom_msg.twist.twist.angular.z = twist_buff.angular.z;
+
+  pub_odom_->publish(odom_msg);
 }
 
 // ========== Velocity CommandをPublishする関数 ========== //

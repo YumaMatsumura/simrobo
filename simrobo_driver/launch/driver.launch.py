ed81285c8fdf8fe95c347929cd0bac3237288ad8
YumaMatsumura <yumapine@gmail.com>
2022-07-28 00:02:20 +0900

    add parameters

--- simrobo_driver/launch/driver.launch.py
+++ simrobo_driver/launch/driver.launch.py
@@ -18,7 +18,9 @@ def generate_launch_description():
         plugin='simrobo_driver::Driver',
         parameters=[
             {'wheel_radius_size_m': 0.1},
-            {'tread_width_m': 0.33}
+            {'tread_width_m': 0.33},
+            {'global_frame_id': 'odom'},
+            {'base_frame_id': 'base_footprint'}
         ]
     )
 

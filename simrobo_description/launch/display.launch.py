0a5561629b0cf71fb5681cea04047a0e5bc9a3f9
YumaMatsumura <yumapine@gmail.com>
2023-03-22 22:56:16 +0900

    update launch files

--- simrobo_description/launch/display.launch.py
+++ simrobo_description/launch/display.launch.py
@@ -2,16 +2,15 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.conditions import IfCondition
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    rviz_dir = os.path.join(get_package_share_directory('simrobo_description'), 'rviz')
-    rviz_file = os.path.join(rviz_dir, 'simrobo.rviz')
+    # Get the rviz directory
+    rviz_file = os.path.join(get_package_share_directory('simrobo_description'), 'rviz', 'simrobo.rviz')
     
     robot_description_content = Command(
         [
@@ -28,42 +27,56 @@ def generate_launch_description():
         ]
     )
     robot_description = {"robot_description": robot_description_content}
-
-
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use simulation (Gazebo) clock if true'
-        ),
-
+    
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_rviz = LaunchConfiguration('use_rviz')
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='True',
+        description='Use simulation (Gazebo) clock if true')
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='True',
+        description='Whether to use rviz')
+        
+    # Create nodes
+    bringup_display_nodes = GroupAction([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[robot_description]
-        ),
+            parameters=[robot_description]),
             
         Node(
             package='joint_state_publisher',
             executable='joint_state_publisher',
             name='joint_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time}]
-        ),
-        
+            parameters=[{'use_sim_time': use_sim_time}]),
+            
         Node(
+            condition=IfCondition(use_rviz),
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
-        ),
-        
+            name='joint_state_publisher_gui'),
+            
         Node(
+            condition=IfCondition(use_rviz),
             package='rviz2',
             executable='rviz2',
             output='screen',
             arguments=['-d', rviz_file],
-            parameters=[{'use_sim_time': use_sim_time}]
-        )
+            parameters=[{'use_sim_time': use_sim_time}])
+            
+        ])
+
+
+    return LaunchDescription([
+        declare_use_sim_time_cmd,
+        
+        declare_use_rviz_cmd,
+
+        bringup_display_nodes
     ])
--- simrobo_gazebo/launch/test_world.launch.py
+++ simrobo_gazebo/launch/test_world.launch.py
@@ -7,20 +7,40 @@ from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch_ros.actions import Node, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
-    display_launch_path = os.path.join(
+    # Get the directory
+    display_launch_file = os.path.join(
         get_package_share_directory('simrobo_description'), 'launch', 'display.launch.py')
-    gzserver_path = os.path.join(
+    gzserver_launch_file = os.path.join(
         get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
-    gzclient_path = os.path.join(
+    gzclient_launch_file = os.path.join(
         get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
-    world_file = 'test_world.world'
-    world_path = os.path.join(
-        get_package_share_directory('simrobo_gazebo'), 'worlds', world_file)
+    world_file_name = 'test_world.world'
+    world_file = os.path.join(
+        get_package_share_directory('simrobo_gazebo'), 'worlds', world_file_name)
+        
+    # Create the launch configuration variables
+    gui = LaunchConfiguration('gui')
+    server = LaunchConfiguration('server')
+    use_rviz = LaunchConfiguration('use_rviz')
+    declare_gui_cmd = DeclareLaunchArgument(
+        'gui',
+        default_value='true',
+        description='Set to "false" to run headless.')
+    declare_server_cmd = DeclareLaunchArgument(
+        'server',
+        default_value='true',
+        description='Set to "false" not to run gzserver.')
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='True',
+        description='Whether to use rviz')
     
+    # Create nodes
     spawn_entity = Node(
         package='gazebo_ros', 
         executable='spawn_entity.py',
@@ -33,45 +53,61 @@ def generate_launch_description():
         output='screen'
     )
     
+    components_node = Node(
+        package='rclcpp_components',
+        executable='component_container',
+        name='simrobo_container',
+        output='screen')
+    
+    load_driver_node = LoadComposableNodes(
+        target_container='simrobo_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='simrobo_driver',
+                plugin='simrobo_driver::Driver',
+                name='simrobo_driver_node',
+                parameters=[
+                    {'wheel_radius_size_m': 0.1},
+                    {'tread_width_m': 0.33},
+                    {'global_frame_id': 'odom'},
+                    {'base_frame_id': 'base_footprint'}])
+        ])
+    
     joint_state_broadcaster = ExecuteProcess(
         cmd=["ros2", "control", "load_controller", "joint_state_broadcaster", 
-             "--set-state", "start"],
+             "--set-state", "active"],
         output="screen"
     )
     
     velocity_controller = ExecuteProcess(
         cmd=["ros2", "control", "load_controller", "velocity_controller", 
-             "--set-state", "start"],
+             "--set-state", "active"],
         output="screen"
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'gui',
-            default_value='true',
-            description='Set to "false" to run headless.'),
-            
-        DeclareLaunchArgument(
-            'server',
-            default_value='true',
-            description='Set to "false" not to run gzserver.'),
+        declare_gui_cmd,
+        
+        declare_server_cmd,
+        
+        declare_use_rviz_cmd,
         
         # ===== display launch (RViz2) ===== #
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(display_launch_path)
+            PythonLaunchDescriptionSource(display_launch_file)
         ),
         
         # ===== Gazebo ===== #
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gzserver_path),
-            launch_arguments={'world': world_path}.items(),
-            condition=IfCondition(LaunchConfiguration('server'))
+            PythonLaunchDescriptionSource(gzserver_launch_file),
+            launch_arguments={'world': world_file}.items(),
+            condition=IfCondition(server)
         ),
         
         IncludeLaunchDescription(
         
-            PythonLaunchDescriptionSource(gzclient_path),
-            condition=IfCondition(LaunchConfiguration('gui'))
+            PythonLaunchDescriptionSource(gzclient_launch_file),
+            condition=IfCondition(gui)
         ),
         
         spawn_entity,
@@ -89,5 +125,10 @@ def generate_launch_description():
                 target_action=joint_state_broadcaster,
                 on_exit=[velocity_controller]
             )
-        )       
+        ),      
+        
+        # ===== Components Nodes ===== #
+        components_node,
+        
+        load_driver_node
     ])

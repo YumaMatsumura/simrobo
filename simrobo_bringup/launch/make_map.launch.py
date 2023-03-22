89711a5143df01e419c8d72c43b176216db30b86
YumaMatsumura <yumapine@gmail.com>
2023-03-22 22:56:28 +0900

    add simrobo_bringup package

---
+++ simrobo_bringup/launch/make_map.launch.py
@@ -0,0 +1,55 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Get the directory
+    params_file = os.path.join(get_package_share_directory('simrobo_bringup'), 'params', 'make_map.yaml')
+    teleop_twist_launch_file = os.path.join(get_package_share_directory('simrobo_bringup'), 'launch', 'teleop_twist_nodes.launch.py')
+    rviz_file = os.path.join(get_package_share_directory('simrobo_bringup'), 'rviz', 'make_map.rviz')
+    
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_keyboard = LaunchConfiguration('use_keyboard')
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+    declare_use_keyboard_cmd = DeclareLaunchArgument(
+        'use_keyboard',
+        default_value='False',
+        description='Whether to use keyboard to control robot')
+    
+    # Create nodes
+    bringup_make_map_nodes = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(teleop_twist_launch_file)),
+        
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[params_file, {'use_sim_time': use_sim_time}]),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', rviz_file],
+            parameters=[{'use_sim_time': use_sim_time}]),
+        
+    ])
+
+    return LaunchDescription([
+        declare_use_sim_time_cmd,
+        declare_use_keyboard_cmd,
+        bringup_make_map_nodes
+    ])
---
+++ simrobo_bringup/launch/teleop_twist_nodes.launch.py
@@ -0,0 +1,50 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Create the launch directory
+    joy_params_file = os.path.join(get_package_share_directory('simrobo_bringup'), 'params', 'joy_params.yaml')
+    
+    # Create the launch configuration variables
+    use_keyboard = LaunchConfiguration('use_keyboard')
+    declare_use_keyboard_cmd = DeclareLaunchArgument(
+      'use_keyboard',
+      default_value='False',
+      description='Whether to use keyboard to control robot')
+    
+    # Create nodes
+    load_joy_nodes = GroupAction(
+        condition=IfCondition(PythonExpression(['not ', use_keyboard])),
+        actions=[
+            Node(
+                package='joy_linux',
+                executable='joy_linux_node',
+                parameters=[{'device_name': '/dev/input/js0'}]),
+        
+            Node(
+                package='teleop_twist_joy',
+                executable='teleop_node',
+                parameters=[joy_params_file])
+            ])
+    
+    load_keyboard_node = Node(
+        condition=IfCondition(use_keyboard),
+        package='teleop_twist_keyboard',
+        executable='teleop_twist_keyboard',
+        output='screen',
+        prefix='xterm -e')
+    
+
+    return LaunchDescription([
+        declare_use_keyboard_cmd,
+        load_joy_nodes,
+        load_keyboard_node
+    ])

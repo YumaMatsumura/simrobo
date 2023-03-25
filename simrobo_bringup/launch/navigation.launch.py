be5a415c7e6e8ab82a7df0eff9df3d3949dc950e
YumaMatsumura <yumapine@gmail.com>
2023-03-25 22:32:37 +0900

    add to execute navigation

---
+++ simrobo_bringup/launch/navigation.launch.py
@@ -0,0 +1,68 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Get the directory
+    nav2_launch_file = os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
+    rviz_file = os.path.join(get_package_share_directory('simrobo_bringup'), 'rviz',  'navigation.rviz')
+    
+    # Create the launch configuration variables
+    map_file = LaunchConfiguration('map')
+    params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_composition = LaunchConfiguration('use_composition')
+    declare_map_file_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=os.path.join(get_package_share_directory('simrobo_bringup'), 'maps', 'sample_map', 'sample_map.yaml'),
+        description='Full path to map yaml file to load')
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(get_package_share_directory('simrobo_bringup'), 'params', 'navigation_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for cleaning nodes')
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        'use_composition', 
+        default_value='True',
+        description='Whether to use composed bringup')
+    
+    # Specify the actions
+    bringup_navigation_nodes = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch_file),
+            launch_arguments={
+                'map': map_file,
+                'params_file': params_file,
+                'use_sim_time': use_sim_time,
+                'use_composition': use_composition}.items()),
+                
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_file],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen')
+    ])
+
+    return LaunchDescription([
+        declare_map_file_cmd,
+        
+        declare_params_file_cmd,
+        
+        declare_use_sim_time_cmd,
+        
+        declare_use_composition_cmd,
+
+        bringup_navigation_nodes
+    ])

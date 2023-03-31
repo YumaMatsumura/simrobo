a9d00b5370c9f02f99725cd02be015a499ca5ba4
YumaMatsumura <yumapine@gmail.com>
2023-03-31 23:00:00 +0900

    #4 add use_composition parameter

---
+++ simrobo_driver/src/driver_node.cpp
@@ -0,0 +1,14 @@
+#include "rclcpp/rclcpp.hpp"
+#include "simrobo_driver/driver_component.hpp"
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::executors::SingleThreadedExecutor exe;
+  auto node = std::make_shared<simrobo_driver::Driver>();
+  exe.add_node(node->get_node_base_interface());
+  exe.spin();
+  rclcpp::shutdown();
+  
+  return 0;
+}
